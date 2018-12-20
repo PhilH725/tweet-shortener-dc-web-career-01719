c4b2ec60420b595be7ed9ce98efa03fe3905b161
Phil Hartley <philh725@gmail.com>
2018-12-20 15:40:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 # Write your code here.
-def dictionary
-  {
+def dictionary(word)
+  dict = {
     "hello" => 'hi' ,
     "to, two, too" => '2' ,
     "for, four" => '4' ,
@@ -9,8 +9,21 @@ def dictionary
     "at" => '@' ,
     "and" => '&'
   }
+  if word in dict
+    dict[word]
+  else
+    word
+  end
 end
 
 def word_substituter(string)
-  
+  string.split(' ')
+  new_string = ''
+  string.each do |word|
+    new_word = dictionary(word)
+    new_string += new_word
+  end
+  new_string
 end
+
+
